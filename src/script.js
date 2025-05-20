ea6b0a6e47150786ac2c0c2d2cf99290959fdd07
Professor <jamesogaygay@gmail.com>
2025-05-20 05:17:30 -1200

    edited the log out page

--- src/script.js
+++ src/script.js
@@ -129,8 +129,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Predefined users (in base64 format for simple obfuscation)
 const users = {
-  "james.gaygay@pypp.org": "cGFzc3dvcmQxMjM=", // password123
-  "alice.doe@pypp.org": "bXlTZWN1cmVQYXNz", // mySecurePass
+  // "james.gaygay@pypp.org": "cGFzc3dvcmQxMjM=", // password123
+  // "alice.doe@pypp.org": "bXlTZWN1cmVQYXNz", // mySecurePass
+
+  pyp2024_20: "cHlwY3hpaV8yMA==", // pypcxii_20
+  pyp2024_21: "cHlwY3hpaV8yMQ==", // pypcxii_21
+  pyp2024_22: "cHlwY3hpaV8yMg==", // pypcxii_22
 };
 
 // Run on page load
